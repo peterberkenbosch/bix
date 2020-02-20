5e1be1ea02c79f7b11d8c16e86c5af409bf61089
Peter Berkenbosch <info@peterberkenbosch.nl>
2020-02-21 00:27:37 +0100

    Testing the contract

---
+++ spec/contracts/users/create_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe Bix::Contracts::Users::Create do
+  context "requires first_name" do
+    let(:input) do
+      {
+        last_name: "Bigg",
+        age: 32
+      }
+    end
+
+    let(:result) { subject.call(input) }
+
+    it "is invalid" do
+      expect(result).to be_failure
+      expect(result.errors[:first_name]).to include("is missing")
+    end
+  end
+
+  context "requires last_name" do
+    let(:input) do
+      {
+        first_name: "Ryan",
+        age: 32
+      }
+    end
+
+    let(:result) { subject.call(input) }
+
+    it "is invalid" do
+      expect(result).to be_failure
+      expect(result.errors[:last_name]).to include("is missing")
+    end
+  end
+
+  context "given valid parameters" do
+    let(:input) do
+      {
+        first_name: "Ryan",
+        last_name: "Bigg",
+        age: 32
+      }
+    end
+
+    let(:result) { subject.call(input) }
+
+    it "is valid" do
+      expect(result).to be_success
+    end
+  end
+end
