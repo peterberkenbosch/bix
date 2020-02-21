c9c2c153b79b79780b136ebd226f7295a3eb9124
Peter Berkenbosch <info@peterberkenbosch.nl>
2020-02-21 10:21:15 +0100

    set auto_registration on the app.config
    no need to grab the default gateway from the config to register the container. The auto_registration has to be outside of the container init block.

--- system/boot/persistence.rb
+++ system/boot/persistence.rb
@@ -1,8 +1,8 @@
 Bix::Application.boot(:persistence) do |app|
   start do
-    container = ROM.container(:sql, app['db.config'].gateways[:default].connection) do |config|
-      config.auto_registration(app.root + 'lib/bix')
-    end
-    register('container', container)
+    config = app['db.config']
+    config.auto_registration(app.root + "lib/bix")
+
+    register('container', ROM.container(app['db.config']))
   end
 end
